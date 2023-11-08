0b4f404e2108e7805f69c485c9b6fa37407f001c
MatthewCai2002 <82549206+MatthewCai2002@users.noreply.github.com>
2023-11-08 01:58:54 -0800

    added insertions for all oter tables

--- api/init-db.sql
+++ api/init-db.sql
@@ -98,8 +98,6 @@ CREATE TABLE appointment(
     specialistID INT,
     endDate DATE NOT NULL,
     serviceName VARCHAR(100) NOT NULL,
-    paymentID INT UNIQUE NOT NULL,
-    reviewID INT UNIQUE,
     UNIQUE(specialistID, startDate),
     FOREIGN KEY (specialistID) REFERENCES specialist(specialistID)
         ON DELETE CASCADE,
@@ -156,7 +154,6 @@ INSERT ALL
     INTO service (serviceName) VALUES ('Make-up')
 SELECT 1 FROM DUAL;
     
-    
 INSERT ALL
     INTO business (businessID, businessName) VALUES (1234, 'ACME Salon')
     INTO business (businessID, businessName) VALUES (5678, 'Inkwell Tattoo Shop')
@@ -174,80 +171,65 @@ INSERT ALL
 SELECT 1 FROM DUAL;
 
 INSERT ALL
-    INTO review (reviewID, reviewMessage, rating, userID, appID) VALUES (1, 'phillip chariot', '123-435-1239', 'philipchariot@gmail.com')
-    INTO review (reviewID, reviewMessage, rating, userID, appID) VALUES (2, 'jared chinatown', '132-421-8123', 'jc@gmail.com')
-    INTO review (reviewID, reviewMessage, rating, userID, appID) VALUES (3, 'alejandro mexico', '213-489-1293', 'am@gmail.com')
-    INTO review (reviewID, reviewMessage, rating, userID, appID) VALUES (4, 'lipa dua', '213-423-4823', 'dualipa@gmail.com')
-    INTO review (reviewID, reviewMessage, rating, userID, appID) VALUES (5, 'chess crackers', '123-483-9123', 'cc@gmail.com')
+    INTO branch (branchID, businessID, branchName, branchAddress, phoneNo, email) VALUES  (1, 1234, 'East Vancouver ACME Salon', '215 Tolmie St', '123-456-7899', 'Eastvanacme@gmail.com') 
+    INTO branch (branchID, businessID, branchName, branchAddress, phoneNo, email) VALUES  (2, 1234, 'Inkwell oakville', '210 North Service Rd W', '101-112-1314', 'oakville@inkwell.com')
+    INTO branch (branchID, businessID, branchName, branchAddress, phoneNo, email) VALUES  (1, 1057, 'The Barber Shop LLoydmaster', '5612 44 St', '123-543-5673', 'thebarbershop@gmail.com' ) 
+    INTO branch (branchID, businessID, branchName, branchAddress, phoneNo, email) VALUES  (1, 5678, 'Smile Thai Kelowna', '1567 Pandosy St', '341-435-1235', 'Smilethaikelowna@yahoo.com') 
+    INTO branch (branchID, businessID, branchName, branchAddress, phoneNo, email) VALUES  (1, 8888, 'Sephora Bromont', '229 de Bromont Boul', '214-543-5673', 'sephora@bromont.com')
+SELECT 1 FROM DUAL
+
+INSERT ALL
+    INTO administrator (userID, adminName, phoneNo, email, branchID, businessID) VALUES (1, 'james Doe', '111-111-1118', 'jamesdoe@hotmail.com', 1, 1234)
+    INTO administrator (userID, adminName, phoneNo, email, branchID, businessID) VALUES (2, 'james cameron', '111-111-5222', 'jamescameron@outlook.com,' 1, 5678)
+    INTO administrator (userID, adminName, phoneNo, email, branchID, businessID) VALUES (3, 'james cordon', '111-222-3353', 'james_cordon@gmail.com', 1, 1057)
+    INTO administrator (userID, adminName, phoneNo, email, branchID, businessID) VALUES (4, 'james pepperoni', '222-333-6444', 'jamespepps@yahoo.com', 2, 1234)
+    INTO administrator (userID, adminName, phoneNo, email, branchID, businessID) VALUES (5, 'reginold leopold the fifth', '555-666-2777', 'regleothe5@regleo.com', 1, 8888)
+SELECT 1 FROM DUAL
+
+INSERT ALL
+    INTO specialist_timeslot_location (startDate, specialistID, branchID, businessID) VALUES ((TO_DATE('17-12-2023 13:30', 'DD-MM-YYYY HH24:MI')), 100, 1, 8888)
+    INTO specialist_timeslot_location (startDate, specialistID, branchID, businessID) VALUES ((TO_DATE('29-09-2023 09:00', 'DD-MM-YYYY HH24:MI')), 101, 1, 1057)
+    INTO specialist_timeslot_location (startDate, specialistID, branchID, businessID) VALUES ((TO_DATE('01-08-2023 11:30', 'DD-MM-YYYY HH24:MI')), 102, 1, 1234)
+    INTO specialist_timeslot_location (startDate, specialistID, branchID, businessID) VALUES ((TO_DATE('01-08-2023 10:30', 'DD-MM-YYYY HH24:MI')), 103, 2, 1234)
+    INTO specialist_timeslot_location (startDate, specialistID, branchID, businessID) VALUES ((TO_DATE('09-07-2023 11:30', 'DD-MM-YYYY HH24:MI')), 103, 1, 5678)
+SELECT 1 FROM DUAL
+
+INSERT ALL
+    INTO specialist_timeslot_customer (startDate, specialistID, userID) VALUES ((TO_DATE('17-12-2023 13:30', 'DD-MM-YYYY HH24:MI')), 100, 1)
+    INTO specialist_timeslot_customer (startDate, specialistID, userID) VALUES ((TO_DATE('29-09-2023 09:00', 'DD-MM-YYYY HH24:MI')), 101, 2)
+    INTO specialist_timeslot_customer (startDate, specialistID, userID) VALUES ((TO_DATE('01-08-2023 11:30', 'DD-MM-YYYY HH24:MI')), 102, 1)
+    INTO specialist_timeslot_customer (startDate, specialistID, userID) VALUES ((TO_DATE('01-08-2023 10:30', 'DD-MM-YYYY HH24:MI')), 103, 4)
+    INTO specialist_timeslot_customer (startDate, specialistID, userID) VALUES ((TO_DATE('09-07-2023 11:30', 'DD-MM-YYYY HH24:MI')), 104, 5)
+SELECT 1 FROM DUAL
+
+INSERT ALL
+    INTO user_payment_method (userID, paymentDate, method) VALUES (1, (TO_DATE('17-12-2023 14:38', 'DD-MM-YYYY HH24:MI')), 'Cash')
+    INTO user_payment_method (userID, paymentDate, method) VALUES (2, (TO_DATE('29-09-2023 10:02', 'DD-MM-YYYY HH24:MI')), 'Debit')
+    INTO user_payment_method (userID, paymentDate, method) VALUES (1, (TO_DATE('01-08-2023 12:32', 'DD-MM-YYYY HH24:MI')), 'Credit')
+    INTO user_payment_method (userID, paymentDate, method) VALUES (4, (TO_DATE('03/08/2023 10:30', 'DD-MM-YYYY HH24:MI')), 'E-Transfer')
+    INTO user_payment_method (userID, paymentDate, method) VALUES (5, (TO_DATE('11/07/2023 14:11', 'DD-MM-YYYY HH24:MI')), 'Credit')
+SELECT 1 FROM DUAL
+
+INSERT ALL
+    INTO appointment (appID, startDate, specialistID, endDate, serviceName) VALUES (1, (TO_DATE('17-12-2023 13:30', 'DD-MM-YYYY HH24:MI')), 100, (TO_DATE('17-12-2023 15:30', 'DD-MM-YYYY HH24:MI')), 'Make-up')
+    INTO appointment (appID, startDate, specialistID, endDate, serviceName) VALUES (2, (TO_DATE('29-09-2023 09:00', 'DD-MM-YYYY HH24:MI')), 101, (TO_DATE('29-09-2023 10:00', 'DD-MM-YYYY HH24:MI')), 'Haircut')
+    INTO appointment (appID, startDate, specialistID, endDate, serviceName) VALUES (3, (TO_DATE('01-08-2023 11:30', 'DD-MM-YYYY HH24:MI')), 102, (TO_DATE('01-08-2023 13:00', 'DD-MM-YYYY HH24:MI')), 'Manicure')
+    INTO appointment (appID, startDate, specialistID, endDate, serviceName) VALUES (4, (TO_DATE('01-08-2023 10:30', 'DD-MM-YYYY HH24:MI')), 103, (TO_DATE('01-08-2023 11:15', 'DD-MM-YYYY HH24:MI')), 'Tattoo')
+    INTO appointment (appID, startDate, specialistID, endDate, serviceName) VALUES (5, (TO_DATE('09-07-2023 11:30', 'DD-MM-YYYY HH24:MI')), 104, (TO_DATE('09-07-2023 12:30', 'DD-MM-YYYY HH24:MI')), 'Tattoo')
 SELECT 1 FROM DUAL;
 
-    (1, 'Awesome experience', 5, 1)
-    (2, 'Not awesome experience', 3, 2)
-    (3, 'Liked this salon', 4, 3)
-    (4, 'Will not come back', 2, 4)
-    (5, 'Not satisfied', 2, 5)
+-- INSERT ALL
+--     INTO review(reviewID, reviewMessage, rating, appID, userID) VALUES (1, 'Awesome experience', 5, 1, 1)
+--     INTO review(reviewID, reviewMessage, rating, appID, userID) VALUES (2, 'Not awesome experience', 3, 2, 2)
+--     INTO review(reviewID, reviewMessage, rating, appID, userID) VALUES (3, 'Liked this salon', 4, 3, 1)
+--     INTO review(reviewID, reviewMessage, rating, appID, userID) VALUES (4, 'Will not come back', 2, 4, 4)
+--     INTO review(reviewID, reviewMessage, rating, appID, userID) VALUES (5, 'Not satisfied', 2, 5, 5)
+-- SELECT 1 FROM DUAL;
 
--- INSERT INTO SpecialistTimeslot_Location(date, specialistID, startTime, branchID, businessID)
--- VALUES
---     ('17/12/2023', 100, '13:30', 1823, 8888),
---     ('29/09/2023', 101, '09:00', 9101, 1057),
---     ('01/08/2023', 102, '11:30', 1234, 1234),
---     ('01/08/2023', 103, '10:30', 5678, 5678),
---     ('09/07/2023', 103, '11:30', 5678, 5678);
-
--- INSERT INTO Appointment (appID, date, specialistID, startTime, endTime, serviceName,
--- paymentID, reviewID)
--- VALUES
---     (1, '17/12/2023', 100, '13:30', '15:30', 'Make-up', 1, 1),
---     (1, '29/09/2023', 101, '09:00', '10:00', 'Haircut', 2, 2),
---     (1, '01/08/2023', 102, '11:30', '13:00', 'Manicure', 3, 3),
---     (1, '01/08/2023', 103, '10:30', '11:15', 'Tattoo', 4, 4),
---     (1, '09/07/2023', 103, '11:30', '12:30', 'Tattoo', 5, 5);
-
--- INSERT INTO SpecialistTimeslot_Customer(date, specialistID, startTime, userID)
--- VALUES
---     ('17/12/2023', 100, '13:30', 1),
---     ('29/09/2023', 101, '09:00', 2),
---     ('01/08/2023', 102, '11:30', 1),
---     ('01/08/2023', 103, '10:30', 4),
---     ('09/07/2023', 103, '11:30', 5);
-
--- INSERT INTO UserPayment_Method(userID, date, time, method)
--- VALUES
---     (1, '17/12/2023', '14:38', “Cash”),
---     (2, '29/09/2023', '10:02', “Debit”),
---     (1, '01/08/2023', '12:32', “Credit”),
---     (4, '03/08/2023', '10:30', “E-Transfer”),
---     (5, '11/07/2023', '14:11', “Credit”);
-
--- INSERT INTO Payment(paymentID, isPaid, price, date, time, userID, appID)
--- VALUES
---     (1, 1, 70.25, '17/12/2023', '13:30', 1, 1),
---     (2, 1, 30.00, '29/09/2023', '09:00', 2, 2),
---     (3, 1, 180.50, '01/08/2023', '11:30', 1, 3),
---     (4, 0, 200.09, '03/08/2023', '10:30', 4, 4),
---     (5, 1, 401.02, '11/07/2023', '11:30', 5, 5);
-
-
--- INSERT
--- INTO Administrator (userID, name, phoneNo, email, branchID, businessID)
--- VALUES
---     (1, james Doe, 111-111-1118, 'jamesdoe@hotmail.com', 1234, 1234)
---     (2, james cameron, 111-111-5222, 'jamescameron@outlook.com,' 5678, 5678)
---     (3, james cordon, 111-222-3353, 'james_cordon@gmail.com', 9101, 1057)
---     (4, james pepperoni , 222-333-6444, 'jamespepps@yahoo.com', 1, 9384)
---     (5, reginold leopold the fifth, 555-666-2777, 'regleothe5@regleo.com', 1823, 8888);
-
--- INSERT
--- INTO Branch(branchID, businessID, name, address, phoneNo, email, adminID)
--- VALUES
---     (1234, 1234, 'East Vancouver ACME Salon', '215 Tolmie St', 123-456-7899,
---     'Eastvanacme@gmail.com', 1)
---     (5678, 5678, 'Inkwell oakville', '210 North Service Rd W', 101-112-1314,
---     'oakville@inkwell.com', 2)
---     (9101, 1057, 'The Barber Shop LLoydmaster', '5612 44 St', 123-543-5673,
---     'thebarbershop@gmail.com' , 3)
---     (1, 9384, 'Smile Thai Kelowna', '1567 Pandosy St', 341-435-1235,
---     'Smilethaikelowna@yahoo.com', 4)
---     (1823, 8888, 'Sephora Bromont' '229 de Bromont Boul', 214-543-5673,
---     'sephora@bromont.com', 5);
+-- INSERT ALL
+--     INTO payment(paymentID, isPaid, price, paymentDate, userID, appID) VALUES (1, 1, 70.25, (TO_DATE('17-12-2023 13:30', 'DD-MM-YYYY HH24:MI')),  1, 1)
+--     INTO payment(paymentID, isPaid, price, paymentDate, userID, appID) VALUES (2, 1, 30.00, (TO_DATE('29-09-2023 09:00', 'DD-MM-YYYY HH24:MI')),  2, 2)
+--     INTO payment(paymentID, isPaid, price, paymentDate, userID, appID) VALUES (3, 1, 180.50, (TO_DATE('01-08-2023 11:30', 'DD-MM-YYYY HH24:MI')), 1, 3)
+--     INTO payment(paymentID, isPaid, price, paymentDate, userID, appID) VALUES (4, 0, 200.09, (TO_DATE('03/08/2023 10:30', 'DD-MM-YYYY HH24:MI')), 4, 4)
+--     INTO payment(paymentID, isPaid, price, paymentDate, userID, appID) VALUES (5, 1, 401.02, (TO_DATE('11/07/2023 11:30', 'DD-MM-YYYY HH24:MI')), 5, 5)
+-- SELECT 1 FROM DUAL;
